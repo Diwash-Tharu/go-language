436878703e293b65088bd5e8fdfdeb77c542a594
Diwash Tharu <diwash.tharu.ch@gmail.com>
2026-09-15 21:09:42 +0545

    setting the database

---
+++ internal/models/models.go
@@ -0,0 +1,43 @@
+package models
+import (
+
+	"fmt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	
+)
+
+type DBModel struct {
+	Order OrderModel
+	User  UserModel
+	DB    *gorm.DB
+}
+
+
+//  connection to the database
+func InitDB(dataSourceName string) (*DBModel, error) {
+	db, err := gorm.Open(sqlite.Open(dataSourceName), &gorm.Config{})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect database: %v", err)
+	}
+	//  thes is migrate 
+	err = db.AutoMigrate(&Order{}, &OrderItem{}, &User{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to migrate database: %v", err)
+	}
+
+	dbModel :=&DBModel{
+		DB: db,
+		OrderModel: OrderModel{
+			BD: db,
+		},
+		User: UserModel{
+			BD: db,
+		},
+		
+	}
+
+
+	return &dbModel, nil
+}
--- internal/models/orger.go
+++ internal/models/orger.go
@@ -5,9 +5,9 @@ import (
 	
 		"fmt"
 		"time"
-
 		"gorm.io/gorm"
-		
+		"github.com/teris-io/shortid"
+
 
 		
 	)
@@ -57,6 +57,55 @@ var (
 
 	type Order struct {
 		ID  string `gorm:"primaryKey;size:14" json:"id"`
+		Status string `gorm:"not null" json:"status"`
+		CustomerName string `gorm:"not null" json:"customerName"`
+		Phone string `gorm:"not null" json:"phone"`
+		Address string `gorm:"not null" json:"address"`
+		Items []OrderItem `gorm:"foreignKey:OrderID" json:"pizzas"`
+		CreatedAt time.Time `json:"createdAt"`
+
+	
+	}
+
+	type OrderItem struct {
+		ID string `gorm:"primaryKey;size:14" json:"id"`
+		OrderID string `gorm:"index;size:14;not nill" json:"orderId"`
+		Size string `gorm:"not null" json:"size"`
+		Pizza string `gorm:"not null" json:"pizza"`
+		Instruction string  `json:"instruction"`
+
 
 	}
+
+	//  checking the order status and if the order status is not in the order status slice then it will return an error
+func (o *Order) BeforeCreated(tx *gorm.DB) error{
+	if o.ID == "" {
+		o.ID = shortid.MustGenerate()
+	}
+	return nil
+}
+
+func (oi *OrderItem) BeforeCreated(tx *gorm.DB) error{
+	if oi.ID == "" {
+		oi.ID = shortid.MustGenerate()
+	}
+	return nil
+}
+func (o *OrderModel) CreateOrder(order *Order) error {
+	//  checking the order status and if the order status is not in the order status slice then it will return an error
+	
+	return o.BD.Create(order).Error
+}
+
+func (o *OrderModel) GetOrder (id string)(*Order, error) {
+	//  checking the order status and if the order status is not in the order status slice then it will return an error
+	
+	var order Order
+	err :=o.BD.Preload("Items").First(&order, "id = ?",id).Error
+	return &order, err
+
+	
+}
+
+
 
